adee99c86109387a3e1e09fa840175b719171cf8
lmos0 <luismarcio.os@outlook.com>
2024-06-19 11:18:27 -0300

    adicionado aula pyautogui2

--- semana_7/bot_dino.py
+++ semana_7/bot_dino.py
@@ -4,7 +4,7 @@ import time
 pyautogui.hotkey('alt', 'tab') #comando para execução de atalhos por meio do teclado
 time.sleep(0.5)
 pyautogui.press('space')
-
+   
 for i in range(100):
     time.sleep(0.8)
     pyautogui.press('space')
