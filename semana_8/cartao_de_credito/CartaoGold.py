2dfb1eae394dd1d6e446076b8a9264e5dbf4a350
lmos0 <luismarcio.os@outlook.com>
2024-06-27 19:29:36 -0300

    aula 27/06 - parcial

--- semana_8/cartao_de_credito/CartaoGold.py
+++ semana_8/cartao_de_credito/CartaoGold.py
@@ -4,10 +4,7 @@ class CartaoGold(CartaodeCredito):
     def __init__(self, numero, nome_titular, data_de_validade, codigo_verificador, limite, bandeira):
         super().__init__(numero, nome_titular, data_de_validade, codigo_verificador, limite, bandeira)
         self.cashback_percentual = 0.03
-        self.milhas_acumuladas = 0
-    
-    def __repr__(self):
-        return f'Milhas acumuladas: {self.milhas_acumuladas}'
+        self.milhas_acumuladas = 0    
     
     def aplicar_cashback(self, valor_da_compra):
         cashback = valor_da_compra * self.cashback_percentual
@@ -19,6 +16,6 @@ class CartaoGold(CartaodeCredito):
         milhas = valor_da_compra / 5
         self.limite -= valor_da_compra
         self.milhas_acumuladas += milhas
-        print(f'Você acumulou {milhas}, seu saldo atual é de {self.acumular_milhas}')
+        print(f'Você acumulou {milhas}, seu saldo atual é de {self.milhas_acumuladas}')
     
     
--- semana_8/cartao_de_credito/main.py
+++ semana_8/cartao_de_credito/main.py
@@ -7,6 +7,7 @@ from CartaoGold import CartaoGold
 cartao1 = CartaodeCredito('554433', "luis", "25/06/2030", "874", 3500.00, "Visa" ) #Instancia e cria objeto do tipo cartão
 cartao2 = CartaoSilver('85965', 'Lucas', '28/02/2029', '410', 8000.00, "American Express" )
 cartao3 = CartaoGold('750284', 'Mariluce', '15/09/2028', '047', 12000.00, 'American Express')
+cartao4 = CartaoGold('98322', 'Marcela', '27/06/2025', '747', 12000.00, 'Mastercard')
 
 #cartao1.consultar_limite() #executa o método inerente à classe CartaodeCredito 
 
@@ -25,3 +26,11 @@ cartao3.pagar_fatura(200)
 cartao3.consultar_limite()
 
 cartao3.acumular_milhas(500)
+
+print('#####################################')
+
+cartao3.consultar_limite()
+cartao4.realizar_compra(599)
+cartao4.aplicar_cashback(4000)
+cartao4.acumular_milhas(6000)
+cartao4.consultar_limite()
